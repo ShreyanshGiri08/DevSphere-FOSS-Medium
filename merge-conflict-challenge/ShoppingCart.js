6161ddc9a1f5fdb0b6f63017c8dcbfaef723f08f
Shreyansh Giri <girilakshmi91@gmail.com>
2026-04-14 19:04:09 +0530

    feat: Add promo code discounts

--- merge-conflict-challenge/ShoppingCart.js
+++ merge-conflict-challenge/ShoppingCart.js
@@ -1,9 +1,17 @@
 class ShoppingCart {
     constructor() {
         this.items = [];
+        this.promoCode = null;
+    }
+
+    applyPromo(code) {
+        this.promoCode = code;
     }
 
     addItem(item) {
+        if (item.price < 0) {
+            throw new Error("Price cannot be negative");
+        }
         this.items.push(item);
     }
 
@@ -12,11 +20,18 @@ class ShoppingCart {
         for (let i = 0; i < this.items.length; i++) {
             total += this.items[i].price;
         }
+        if (this.promoCode === 'SAVE10') {
+            total *= 0.9;
+        } else if (this.promoCode === 'SAVE20') {
+            total *= 0.8;
+        }
         return total;
     }
 
     generateReceipt() {
-        return `Total: $${this.calculateTotal()}`;
+        let receipt = `Items: ${this.items.length}\n`;
+        receipt += `Total: $${this.calculateTotal().toFixed(2)}`;
+        return receipt;
     }
 }
 
--- merge-conflict-challenge/index.js
+++ merge-conflict-challenge/index.js
@@ -1,5 +1,6 @@
 const ShoppingCart = require('./ShoppingCart');
 
 const cart = new ShoppingCart();
+cart.applyPromo('SAVE20');
 cart.addItem({ name: "Laptop", price: 1000 });
 console.log(cart.generateReceipt());
