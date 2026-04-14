f4b1e0781c9f5acf25e5f4c376c8ea039ed26631
Shreyansh Giri <girilakshmi91@gmail.com>
2026-04-14 19:04:57 +0530

    feat: Add regional taxes and shipping

--- merge-conflict-challenge/ShoppingCart.js
+++ merge-conflict-challenge/ShoppingCart.js
@@ -1,22 +1,40 @@
 class ShoppingCart {
     constructor() {
         this.items = [];
+        this.currency = 'USD';
     }
 
     addItem(item) {
+        if (!item.name) {
+            throw new Error("Item must have a name");
+        }
         this.items.push(item);
+    }
+
+    setCurrency(currency) {
+        this.currency = currency;
     }
 
     calculateTotal() {
         let total = 0;
         for (let i = 0; i < this.items.length; i++) {
-            total += this.items[i].price;
+            total += this.items[i].price; // Assumes USD
         }
+        
+        // Apply 8% tax
+        total *= 1.08;
+        
+        // Add $5 flat shipping
+        total += 5;
+        
         return total;
     }
 
     generateReceipt() {
-        return `Total: $${this.calculateTotal()}`;
+        let receipt = `Order Receipt (${this.currency})\n`;
+        receipt += "------------------\n";
+        receipt += `Final Total: ${this.currency} ${this.calculateTotal().toFixed(2)}`;
+        return receipt;
     }
 }
 
--- merge-conflict-challenge/index.js
+++ merge-conflict-challenge/index.js
@@ -1,5 +1,6 @@
 const ShoppingCart = require('./ShoppingCart');
 
 const cart = new ShoppingCart();
+cart.setCurrency('USD');
 cart.addItem({ name: "Laptop", price: 1000 });
 console.log(cart.generateReceipt());
